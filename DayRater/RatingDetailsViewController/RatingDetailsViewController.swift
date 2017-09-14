81a7c357135b3698487e7e70b5b47b35ecfa9e7f
Michael Chan <michael-za.chan@ubs.com>
2017-09-14 17:42:46 +0800

    Added RatingDetailsViewController files into the project and storyboard

---
+++ DayRater/RatingDetailsViewController/RatingDetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RatingDetailsViewController.swift
+//  DayRater
+//
+//  Created by Michael on 14/9/2017.
+//  Copyright © 2017 Michael. All rights reserved.
+//
+
+import UIKit
+
+class RatingDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var containerView: UIView!
+    
+    var ratingLabel: UILabel!
+    var descriptionTextView: UITextView!
+    
+    
+    
+}
